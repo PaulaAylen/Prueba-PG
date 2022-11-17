69ce00526e61166e050943f67e2eb9dd9be7e600
Gino <ginolencina@gmail.com>
2022-11-16 23:51:02 -0300

    intento 2 de subir

---
+++ ejercitacionGino.js
@@ -0,0 +1,20 @@
+let saludo = "hola";
+console.log(saludo)
+
+{
+    let saludo = "chau";
+    console.log(saludo)
+}
+
+let age = 22;
+age * 2;
+console.log(age)
+age = age * "2";
+console.log(age)
+
+let nombre = 'Gino';
+let apellido = 'Lencina';
+console.log(nombre + " " + apellido);
+
+//como saber el tipo de una variable
+console.log(typeof age);
